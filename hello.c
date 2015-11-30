c4802e858fb2480894d6b048299945fa0c6fad5b
Kalen Tang <kalentang@hotmail.com>
2015-11-30 15:43:02 +0800

    add the Third line

--- hello.c
+++ hello.c
@@ -3,7 +3,8 @@
 int main(int args,char *argv[])
 {
 	printf("Hello world\n");
-	printf("This is the Second line");
+	printf("This is the Second line\n");
+	printf("This is the Third line.\n");
 	getchar();
 	return 0;
 }
