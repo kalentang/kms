41076da539049b71d1a8c751b72e551e98ef8ea5
Kalen Tang <kalentang@hotmail.com>
2015-12-08 17:17:18 +0800

    a updated line.

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@ int main(int args,char *argv[])
 	printf("Hello world\n");
 	printf("This is the Second line\n");
 	printf("This is the Third line.\n");
+	printf("This is the a updated line.\n");
 	getchar();
 	return 0;
 }
