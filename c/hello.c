704fca4d3f5beb581f1a6b608a1e5abdc29e4ee2
Kalen Tang <kalentang@hotmail.com>
2016-11-20 00:40:17 +0800

    add one test line

--- c/hello.c
+++ c/hello.c
@@ -6,6 +6,7 @@ int main(int args,char *argv[])
 	printf("This is the Second line\n");
 	printf("This is the Third line.\n");
 	printf("This is the updated line.\n");
+	printf("This is the test line.\n");
 	getchar();
 	return 0;
 }
